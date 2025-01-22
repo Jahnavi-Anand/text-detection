8137d85c7aa78a2410d7c1294bf4f5ca944a967d
Jahnavi Anand <jahnavianandxib@gmail.com>
2025-01-22 15:48:09 +0530

    Update verify.py

--- verify.py
+++ verify.py
@@ -2,7 +2,7 @@ import pytesseract
 from PIL import Image
 
 # Make sure the Tesseract executable is accessible
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'  # Update the path as needed
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'  # Update the path as needed
 
 # Test OCR on an image
 image = Image.open('example.png')  # Replace with your image file
